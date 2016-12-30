de7c0d2d512c94bc835568526fcd1868cb79c8a7
alisuehobbs <alisuehobbs@gmail.com>
2016-12-30 13:20:21 -0700

    added accept cors to app.js

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/signup', signup);
 app.use('/login', login);
